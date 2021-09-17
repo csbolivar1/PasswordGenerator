b7df18f89482e3a09559a0d8f099131053b03c74
csbolivar1 <christopher.s.bolivar@gmail.com>
2021-09-16 21:54:45 -0400

    Generate password with upper and lowercase letters by default

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -4,6 +4,7 @@ import string
 import time
 
 root = Tk()
+password = ''
 
 def generatePassword():
     copyConfirm.config(text="") # TODO: temp solution, should be in copyPassword function
@@ -15,17 +16,14 @@ def copyPassword():
     root.clipboard_append(passwordLabel['text'])
     copyConfirm.config(text="Password Copied")
     
-    
-passwordLabel = Label(root, text="filler")                       
+clicked = IntVar()
+clicked.set(5)
+
+passwordLabel = Label(root, text=password.join(random.choice(string.ascii_letters) for x in range(clicked.get())))                       
 generatePasswordBtn = Button(root, text="Generate Password", command=generatePassword)
 copyPasswordBtn = Button(root, text="Copy Password", command=copyPassword)
 copyConfirm = Label(root, text="")
 
-clicked = IntVar()
-clicked.set(5)
-
-upperCaseChkBtn = Checkbutton(root, text="Include Uppercase Letters (A-Z)")
-lowerCaseChkBtn = Checkbutton(root, text="Include Lowercase Letters (a-z)")
 numberChkBtn = Checkbutton(root, text="Include Numbers (0-9)")
 specialCharChkBtn = Checkbutton(root, text="Include Special Characters")
 passwordLength = OptionMenu(root, clicked, 5, 6, 7, 8, 9, 10)
@@ -34,8 +32,6 @@ passwordLabel.pack()
 generatePasswordBtn.pack()
 copyPasswordBtn.pack()
 copyConfirm.pack()
-upperCaseChkBtn.pack()
-lowerCaseChkBtn.pack()
 numberChkBtn.pack()
 specialCharChkBtn.pack()
 passwordLength.pack()
