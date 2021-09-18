6854c771ff2229ae8bdacff4bb5f811c193ca260
csbolivar1 <christopher.s.bolivar@gmail.com>
2021-09-18 19:01:56 -0400

    Included numbers in password

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,14 +1,24 @@
 from tkinter import *
 import random
 import string
-import time
 
 root = Tk()
 password = ''
 
+clicked = IntVar()
+clicked.set(5)
+
+numchk = IntVar()
+
 def generatePassword():
     copyConfirm.config(text="") # TODO: temp solution, should be in copyPassword function
     password = ''.join(random.choice(string.ascii_letters) for x in range(clicked.get()))
+
+    # TODO: add numbers to password generation
+
+    if numchk.get() == 1:
+        password = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(clicked.get()))
+
     passwordLabel.config(text=password)
 
 def copyPassword():
@@ -16,16 +26,13 @@ def copyPassword():
     root.clipboard_append(passwordLabel['text'])
     copyConfirm.config(text="Password Copied")
     
-clicked = IntVar()
-clicked.set(5)
-
 passwordLabel = Label(root, text=password.join(random.choice(string.ascii_letters) for x in range(clicked.get())))                       
 generatePasswordBtn = Button(root, text="Generate Password", command=generatePassword)
 copyPasswordBtn = Button(root, text="Copy Password", command=copyPassword)
 copyConfirm = Label(root, text="")
 
-numberChkBtn = Checkbutton(root, text="Include Numbers (0-9)")
-specialCharChkBtn = Checkbutton(root, text="Include Special Characters")
+numberChkBtn = Checkbutton(root, text="Include Numbers (0-9)", variable=numchk, onvalue=1, offvalue=0)
+# specialCharChkBtn = Checkbutton(root, text="Include Special Characters")
 passwordLength = OptionMenu(root, clicked, 5, 6, 7, 8, 9, 10)
 
 passwordLabel.pack()
@@ -33,7 +40,7 @@ generatePasswordBtn.pack()
 copyPasswordBtn.pack()
 copyConfirm.pack()
 numberChkBtn.pack()
-specialCharChkBtn.pack()
+# specialCharChkBtn.pack()
 passwordLength.pack()
 
 root.mainloop()
